c25651f89dfb3f1508ce55f68ca447fb602209a5
NAHO <90870942+trueNAHO@users.noreply.github.com>
2025-01-14 18:02:06 +0100

    macros: benchmark with various input file sizes

--- crates/lib/src/macros.rs
+++ crates/lib/src/macros.rs
@@ -1,18 +1,34 @@
 #[macro_export]
 macro_rules! bench {
     ($type:ty) => {
-        use criterion::{criterion_group, criterion_main, Criterion};
+        use criterion::{
+            criterion_group, criterion_main, BenchmarkId, Criterion,
+        };
+
         use lib::Solution;
 
         pub fn benchmark(c: &mut Criterion) {
-            c.bench_function(
-                stringify!($type).split("::").next().unwrap(),
-                |b| {
-                    b.iter(|| {
-                        <$type>::solve(&<$type>::input_file(<$type>::SIZE))
-                    })
-                },
-            );
+            let mut group = c
+                .benchmark_group(stringify!($type).split("::").next().unwrap());
+
+            for size in [
+                1e4 as isize,
+                1e5 as isize,
+                1e6 as isize,
+                1e7 as isize,
+                1e8 as isize,
+                1e9 as isize,
+            ] {
+                group.bench_with_input(
+                    BenchmarkId::from_parameter(size),
+                    &size,
+                    |b, &size| {
+                        b.iter(|| <$type>::solve(&<$type>::input_file(size)))
+                    },
+                );
+            }
+
+            group.finish();
         }
 
         criterion_group!(benches, benchmark);
